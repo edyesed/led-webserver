5f9f1634f79a717c17e5883c216c855255829863
Ed Anderson <ed@realself.com>
2015-09-04 10:31:24 -0700

    go and bridge the old gods (serial) and the new (http)

---
+++ README.md
@@ -0,0 +1,10 @@
+# flask nonsense that manages a arduino led
+Yep. that's what it does. 
+
+# You'll want this loaded on your arduino
+[https://github.com/edyesed/RGBLed](What to load on the arduino)
+
+# requirements
+* python
+* flask
+* pyserial
---
+++ bin/server.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+#
+import serial
+import logging
+import json
+from time import sleep
+from flask import Flask
+
+from logging.handlers import RotatingFileHandler
+
+app = Flask(__name__)
+currcolor = None
+
+ser = serial.Serial('/dev/cu.Repleo-CH341-00001114', timeout=5)
+connected = False
+currentcolor = None
+
+@app.route("/")
+def index():
+  return "led server is go"
+
+@app.route('/color/<color_set>', methods=['POST'])
+def set_color(color_set=None):
+  #return 'Color is %s' % color_set
+  app.logger.debug("color is {0}".format(color_set))
+  for i in range(1,3):
+    errored = False
+    app.logger.debug("range is {0}".format(i))
+    cool_story_bro = color_set + "\n"
+    try:
+      ser.write(str(cool_story_bro))
+    except Exception as e:
+      errored = True
+      print("ERR ser.write newline: %s" % (e))
+    try:
+      ser.readline(13)
+    except Exception as e:
+      errored = True
+      print("ERR ser.readline 2: %s" % (e))
+    if errored is not True:
+      break
+    sleep(2)
+  retinfo = {}
+  retinfo['color'] = color_set
+  return(json.dumps(retinfo))
+
+if __name__ == "__main__":
+  while not connected:
+    serin = ser.read()
+    connected = True
+  # just go ahead and read anything that is already on the serial port ( which 
+  # SHOULD be nothing, but it's serial. so there's that. 
+  ser.readline(13)
+  handler = RotatingFileHandler('server.log', maxBytes=10000, backupCount=1)
+  handler.setLevel(logging.DEBUG)
+  app.logger.addHandler(handler)
+  app.run()
